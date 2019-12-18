e32b8d8751b7b2b557c1b4a29534cf7114369a18
Clement Albanel <clement.albanel@gmail.com>
2019-12-18 13:57:16 +0100

    Donnée de test pour entreprise avec faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,14 +12,28 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        $entrepriseMCB = new Entreprise();
-        $entrepriseMCB->setNom("MCB");
-        $entrepriseMCB->setActivite("Que des bgs");
-        $entrepriseMCB->setAdresse("10bis rue de mirambeau");
-        $entrepriseMCB->setSiteWeb("www.mcblpb.com");
-        $entrepriseMCB->setEmail("mcb@gmail.com");
+    	$faker = \Faker\Factory::create('fr_FR');
 
-        $manager->persist($entrepriseMCB);
+        //$entrepriseMCB = new Entreprise();
+        //$entrepriseMCB->setNom("MCB");
+        //$entrepriseMCB->setActivite("Que des bgs");
+        //$entrepriseMCB->setAdresse("10bis rue de mirambeau");
+        //$entrepriseMCB->setSiteWeb("www.mcblpb.com");
+        //$entrepriseMCB->setEmail("mcb@gmail.com");
+
+        //$manager->persist($entrepriseMCB);
+
+        $entreprise = [];
+        for ($i=0; $i < 10; $i++) {
+            $entreprise[$i] = new Entreprise();
+            $entreprise[$i]->setNom($faker->company)
+                ->setActivite($faker->realText($maxNbChars = 200, $indexSize = 2))
+                ->setAdresse($faker->address)
+                ->setSiteWeb($faker->url)
+                ->setEmail($faker->companyEmail)
+            ;
+            $manager->persist($entreprise[$i]);
+        }
 
         $manager->flush();
     }
