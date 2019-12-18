0b5186e6c3887855c7936aee555aca234e7ae89e
Clement Albanel <clement.albanel@gmail.com>
2019-12-18 13:33:21 +0100

    Donnée de test pour entreprise

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Entreprise;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $entrepriseMCB = new Entreprise();
+        $entrepriseMCB->setNom("MCB");
+        $entrepriseMCB->setActivite("Que des bgs");
+        $entrepriseMCB->setAdresse("10bis rue de mirambeau");
+        $entrepriseMCB->setSiteWeb("www.mcblpb.com");
+        $entrepriseMCB->setEmail("mcb@gmail.com");
+
+        $manager->persist($entrepriseMCB);
+
+        $manager->flush();
+    }
+}
