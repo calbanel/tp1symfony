b536052f82409dbade611ad5430ff416485decb3
Clement Albanel <clement.albanel@gmail.com>
2019-12-18 13:47:07 +0100

    Ajout de Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use Faker;
 
 class AppFixtures extends Fixture
 {
